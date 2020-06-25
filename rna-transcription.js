082dacd5eb43a8a9ba5d9cc08bb81697a33d29f7
KamariRoss <kamarimobile@gmail.com>
2020-06-25 14:08:05 -0400

    Final edit

--- rna-transcription.js
+++ rna-transcription.js
@@ -10,6 +10,7 @@
 export const toRna = (RNA) => {
   let rnaArray = RNA.split("")
   const TRANSCRIPTION = { A: "U", T: "A", C: "G", G: "C", U: "T" }
+
   for (let index = 0; index < rnaArray.length; index++) {
     if (TRANSCRIPTION[rnaArray[index]] || TRANSCRIPTION[rnaArray[index]]) {
       if (TRANSCRIPTION[rnaArray[index]]) {
